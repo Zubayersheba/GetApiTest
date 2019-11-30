f9359f44fd14cbc77b082d19c241670e263a8391
zubayersheba <zubayersheba18@gmail.com>
2019-11-30 17:33:42 +0600

    [Live Api update]

--- src/main/java/com/restapi/client/Restclient.java
+++ src/main/java/com/restapi/client/Restclient.java
@@ -11,10 +11,12 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Restclient {
     //Get method
    // int statuscode;
+    // without header
     public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(url);
@@ -26,4 +28,19 @@ public class Restclient {
         return closeableHttpResponse;
 
     }
+    // with header
+    /*public CloseableHttpResponse get(String url, HashMap<String, String> hashMap) throws ClientProtocolException, IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        for(Map.Entry<String, String> entry: headerMap.entrySet()){
+            httpGet.addHeader(entry.getKey(), entry.getValue());
+        }
+        CloseableHttpResponse closeableHttpResponse = httpClient.execute(httpGet);//hit the get url
+        //json string
+       /* String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+        JSONObject responseJson=new JSONObject(responseString);
+        System.out.println("--Json objects---->" + responseJson);
+        return closeableHttpResponse;
+
+    }*/
 }
--- src/test/java/GetApiTest/GetApiTest.java
+++ src/test/java/GetApiTest/GetApiTest.java
@@ -13,6 +13,7 @@ import util.com.testapi.TestUtil;
 
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class GetApiTest extends TestBase {
     TestBase testbase;
@@ -38,15 +39,72 @@ public class GetApiTest extends TestBase {
         restclient.get(url1);*/
 
     }
-    @Test
+  /*  @Test
     public void getApiTest() throws ClientProtocolException,IOException {
          restclient=new Restclient();
          restclient.get(url1);
        // System.out.println(url1);
 
-    }
+    }*/
+  /*public void addresses() throws IOException {
+      String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+      JSONObject responseJson=new JSONObject(responseString);
+
+      String address= TestUtil.getValueByJPath(responseJson, "/addresses[0]/address");
+      String id= TestUtil.getValueByJPath(responseJson, "/addresses[0]/id");
+      String location_id= TestUtil.getValueByJPath(responseJson, "/addresses[0]/location_id");
+      String name= TestUtil.getValueByJPath(responseJson, "/addresses[0]/name");
+      String flat_no= TestUtil.getValueByJPath(responseJson, "/addresses[0]/flat_no");
+      String geo_lng= TestUtil.getValueByJPath(responseJson, "/addresses[0]/geo_informations/lng");
+      String geo_lat= TestUtil.getValueByJPath(responseJson, "/addresses[0]/geo_informations/lat");
+
+      System.out.println("--ID---->" + id);
+      System.out.println("--Location ID---->" + location_id);
+      System.out.println("--Address---->" + address);
+      System.out.println("--Address Name---->" + name);
+      System.out.println("--Flat No---->" + flat_no);
+      System.out.println("--Geo_informations lng---->" + geo_lng);
+      System.out.println("--Geo_informations lat---->" + geo_lat);
+
+  }*/
     @Test
     public void getApiTestSheba() throws ClientProtocolException,IOException {
+        restclient=new Restclient();
+        closeableHttpResponse = restclient.get(url1sheba);
+        //Status code
+        int statuscode= closeableHttpResponse.getStatusLine().getStatusCode();
+        System.out.println("---Status code----->" + statuscode);
+        //Assert.assertEquals(statuscode, Response_code_201, "Status code is not 200");
+        Assert.assertEquals("Status code is not 200",Response_code_200,statuscode);
+
+       //json string
+        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+        JSONObject responseJson=new JSONObject(responseString);
+        System.out.println("--Json objects---->" + responseJson);
+
+
+
+        // get value from address json array
+        System.out.println("\n---------------------- \n" );
+
+        String address= TestUtil.getValueByJPath(responseJson, "/addresses[0]/address");
+        String id= TestUtil.getValueByJPath(responseJson, "/addresses[0]/id");
+        String location_id= TestUtil.getValueByJPath(responseJson, "/addresses[0]/location_id");
+        String name= TestUtil.getValueByJPath(responseJson, "/addresses[0]/name");
+        String flat_no= TestUtil.getValueByJPath(responseJson, "/addresses[0]/flat_no");
+        String geo_lng= TestUtil.getValueByJPath(responseJson, "/addresses[0]/geo_informations/lng");
+        String geo_lat= TestUtil.getValueByJPath(responseJson, "/addresses[0]/geo_informations/lat");
+
+        System.out.println("--ID---->" + id);
+        System.out.println("--Location ID---->" + location_id);
+        System.out.println("--Address---->" + address);
+        System.out.println("--Address Name---->" + name);
+        System.out.println("--Flat No---->" + flat_no);
+        System.out.println("--Geo_informations lng---->" + geo_lng);
+        System.out.println("--Geo_informations lat---->" + geo_lat);
+
+    }
+    /*public void getApiTestSheba() throws ClientProtocolException,IOException {
         restclient=new Restclient();
          closeableHttpResponse = restclient.get(url1);
          //Status code
@@ -74,8 +132,19 @@ public class GetApiTest extends TestBase {
 
         // get value from json array
 
+       String last_name= TestUtil.getValueByJPath(responseJson, "/data[0]/last_name");
+       String id= TestUtil.getValueByJPath(responseJson, "/data[0]/id");
+       String first_name= TestUtil.getValueByJPath(responseJson, "/data[0]/first_name");
+       String avatar= TestUtil.getValueByJPath(responseJson, "/data[0]/avatar");
+       String email= TestUtil.getValueByJPath(responseJson, "/data[0]/email");
+
+        System.out.println("--ID---->" + id);
+        System.out.println("--Email---->" + email);
+        System.out.println("--First Name---->" + first_name);
+        System.out.println("--Last Name---->" + last_name);
+        System.out.println("--Avatar---->" + avatar);
+
+    }*/
 
 
-
-    }
 }
